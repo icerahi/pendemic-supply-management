11071755efa8cac000ff4e47e0e8d58c84712fd3
rahi <zanjarwhite@gmail.com>
2021-08-05 15:31:03 +0600

    stock validation done ,request validation required

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,19 +1,115 @@
 from django.contrib import admin
-from .models import Stock, Request
+from .models import Stock, Request, Equipment
+from django.core.exceptions import ValidationError
+from django.contrib import messages
+from django import forms
+from django.contrib.auth import get_user_model
 # Register your models here.
+User = get_user_model()
+
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image_tag',)
+
+
+class RequestForm(forms.ModelForm):
+    class Meta:
+        model = Request
+        fields = '__all__'
+
+    def clean(self):
+        status = self.cleaned_data.get('accepted')
+        equipment = self.cleaned_data.get('equipment')
+        quantity = self.cleaned_data.get('quantity')
+
+        if status == True:
+            superuser = User.objects.get(is_superuser=True)
+            superuser_equipment = superuser.stock_set.all().get(equipment=equipment)
+
+            if quantity <= superuser_equipment.quantity:
+                superuser_equipment.quantity -= quantity
+                superuser_equipment.save()
+
+            else:
+
+                raise forms.ValidationError(
+                    f'Stock over! You "{equipment}" quantity is {superuser_equipment.quantity}.')
+            return self.cleaned_data
+        return self.cleaned_data
 
 
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
-    list_display = ('user', 'equipment', 'message', 'created', 'accepted')
+    form = RequestForm
+    list_display = ('user', 'equipment', 'image_tag', 'quantity',
+                    'message', 'created', 'accepted')
     list_display_links = ('user', 'equipment', 'message',
-                          'created', 'accepted')
-    search_fields = ('user,equipment', 'message')
+                          'created',)
+    search_fields = ('user__username', 'equipment__name', 'message')
+    list_filter = (('user__username'), ('equipment__name'))
+
+    # def save_model(self, request, obj, form, change):
+
+    #     if obj.accepted == True:
+    #         superuser_equipment = request.user.stock_set.all().get(
+    #             equipment=obj.equipment)
+    #         if obj.quantity <= superuser_equipment.quantity:
+    #             superuser_equipment.quantity -= obj.quantity
+    #             superuser_equipment.save()
+    #             return super().save_model(request, obj, form, change)
+    #         else:
+    #             messages.error(request, 'Central Stock Over!')
+    #             obj.accepted = False
+    #     return super().save_model(request, obj, form, change)
+
+
+class StockForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = '__all__'
+
+    def clean(self):
+        user = self.cleaned_data.get('user')
+        if user.is_superuser is not True:
+            equipment = self.cleaned_data.get('equipment')
+            quantity = self.cleaned_data.get('quantity')
+            superuser = User.objects.get(is_superuser=True)
+            try:
+                superuser_equipment = superuser.stock_set.all().get(equipment=equipment)
+                if quantity <= superuser_equipment.quantity:
+                    superuser_equipment.quantity -= quantity
+                    superuser_equipment.save()
+                else:
+                    raise forms.ValidationError(
+                        f'Stock over! You "{equipment}" quantity is {superuser_equipment.quantity}.')
+                return self.cleaned_data
+            except:
+                raise forms.ValidationError(
+                    f'Stock of "{equipment}" not added yet in Admin!"')
+        return self.cleaned_data
 
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
-    list_display = ('user', 'name', 'image', 'quantity', 'created')
+    form = StockForm
+    list_display = ('user', 'equipment', 'image_tag', 'quantity', 'created')
+    list_display_links = ('user', 'equipment', 'quantity', 'created')
+    search_fields = ('user__username', 'equipment__name',)
+    list_filter = (('user__username'), ('equipment__name'),)
+
+    # def save_model(self, request, obj, form, change):
+    #     if obj.user != request.user:
+    #         # print(equipment)
+    #         superuser_equipment = request.user.stock_set.all().get(
+    #             equipment=obj.equipment)
+    #         if obj.quantity < superuser_equipment.quantity:
+    #             superuser_equipment.quantity -= obj.quantity
+    #             superuser_equipment.save()
+    #             return super().save_model(request, obj, form, change)
+
+    #         else:
+    #             messages.error(request, 'Central Stock Over')
 
 
 admin.site.site_header = "Pendemic Supply Management"
--- stocks/models.py
+++ stocks/models.py
@@ -1,35 +1,75 @@
 from django.db import models
 from django.conf import settings
-from django.db.models.fields import BooleanField
+from django.utils.safestring import mark_safe
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
+from django.contrib.auth import get_user_model
+from django.contrib import messages
 # Create your models here.
+User = get_user_model()
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to="equipment_img")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '  Equipment'
+
+    @mark_safe
+    def image_tag(self):
+        if self.image:
+            return '<a href="{}"><img src="{}" style="width: 30px; height:30px;" /></a>'.format(self.image.url, self.image.url)
+        else:
+            return 'No Image Found'
+
+    image_tag.short_description = 'Image'
 
 
 class Stock(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="equipment")
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.equipment.name
 
     class Meta:
-        verbose_name = 'Equipments Stock'
+        verbose_name = ' Equipments Stock'
         ordering = ['-updated']
+        unique_together = ("user", "equipment")
+        # constraints = [
+        #     models.UniqueConstraint(
+        #         fields=['user', 'equipment', ],
+        #         name="Can't create multiple stock of same equipment,So update the existing equipment's quantity"
+        #     ),
+        # ]
+
+    @mark_safe
+    def image_tag(self):
+        if self.equipment.image:
+            return '<a href="{}"><img src="{}" style="width: 30px; height:30px;" /></a>'.format(self.equipment.image.url, self.equipment.image.url)
+        else:
+            return 'No Image Found'
+
+    image_tag.short_description = 'Image'
 
 
 class Request(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    equipment = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
     message = models.TextField()
-    accepted = BooleanField(default=False)
+    accepted = models.BooleanField(default=False)
 
     created = models.DateTimeField(auto_now_add=True)
 
@@ -39,3 +79,12 @@ class Request(models.Model):
     class Meta:
         verbose_name = 'Equipment Request'
         ordering = ['-created']
+
+    @mark_safe
+    def image_tag(self):
+        if self.equipment.image:
+            return '<a href="{}"><img src="{}" style="width: 30px; height:30px;" /></a>'.format(self.equipment.image.url, self.equipment.image.url)
+        else:
+            return 'No Image Found'
+
+    image_tag.short_description = 'Image'
